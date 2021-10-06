5a451143e23c3ba6c969cbcd6280bd574b53b84c
Rktify <rktify@gmail.com>
2021-10-06 14:12:15 +0700

    Finished no1

---
+++ Sir Jude/Assignment 1/Slope of a line.py
@@ -0,0 +1,8 @@
+x1 = eval(input("Enter the x-coordinate for point 1: "))
+x2 = eval(input("Enter the x-coordinate for point 2: "))
+y1 = eval(input("Enter the y-coordinate for point 1: "))
+y2 = eval(input("Enter the y-coordinate for point 2: "))
+
+slope = (y2-y1)/(x2-x1)
+
+print(f"The slope for the line that connects the 2 points ({x1}, {x2}) and ({y1}, {y2}) is %2.5f" % (slope))
