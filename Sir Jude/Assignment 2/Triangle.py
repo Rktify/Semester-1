1b4340657f32bcb3bf00009cdfa1fa3e078f4d12
Rktify <rktify@gmail.com>
2021-10-06 14:23:00 +0700

    Updated

--- Sir Jude/Assignment 2/Triangle.py
+++ Sir Jude/Assignment 2/Triangle.py
@@ -3,6 +3,6 @@ edge2 = eval(input("Enter the length of the 2nd edge of the triangle : "))
 edge3 = eval(input("Enter the length of the 3rd edge of the triangle : "))
 if(edge1+edge2 >= edge3) and (edge1+edge3 >= edge2) and (edge2+edge3 >= edge1):
     perimeter = edge1+edge2+edge3
-    print("The perimeter of your triangle is", perimeter)
+    print(f"The perimeter of your triangle is {perimeter}")
 else:
     print("The perimeter cannot be calculated. Your input is invalid, please retry")
