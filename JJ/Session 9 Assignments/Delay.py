b6d1b5504d8403512443e693fc76e8a197163f9a
Rktify <rktify@gmail.com>
2021-10-27 19:06:11 +0700

    Finished

---
+++ JJ/Session 9 Assignments/Delay.py
@@ -0,0 +1,10 @@
+
+def delay(string,te):
+    import time
+    time.sleep(te)
+    print(string)
+
+msg = input("What message do you want to be printed?: ")
+tm = int(input("How long do you want the delay to be?: "))
+
+delay(msg,tm)
